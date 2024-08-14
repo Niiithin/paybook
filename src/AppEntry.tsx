1d4524e40e2d62b925b6079f0893ffcbcf73d741
Nithin <nithibl19@gmail.com>
2024-08-14 15:55:14 +0530

    first commit

---
+++ src/AppEntry.tsx
@@ -0,0 +1,10 @@
+/* Imports */
+import React, {useEffect, useRef, useState} from 'react';
+import {Text} from 'react-native';
+import NoInternet from './components/NoInternet';
+
+const AppEntry = () => {
+  return <Text>AppEntry</Text>;
+};
+
+export default AppEntry;
